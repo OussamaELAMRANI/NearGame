33861b6e8208ba16f9eaedf89d196b9bd0968202
Oussama EL AMRANI <oelamrani@sql.com>
2021-10-16 16:47:32 +0100

    feat(env): setup debuger and profiler

--- src/Http/Controller/AppController.php
+++ src/Http/Controller/AppController.php
@@ -3,6 +3,7 @@
 namespace App\Http\Controller;
 
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
 
@@ -14,5 +15,15 @@ class AppController extends AbstractController
     public function index(): Response
     {
         return $this->json(['data' => 'App'], Response::HTTP_OK);
+    }
+
+    /**
+     * @Route("/profiler", name="profiler_test")
+     */
+    public function testProfiler(Request $request): Response
+    {
+        dump($request);
+
+        return $this->render('base.html.twig');
     }
 }
